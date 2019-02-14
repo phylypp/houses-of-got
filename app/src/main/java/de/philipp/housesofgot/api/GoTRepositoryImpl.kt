5ee3ab7dccb1858c93dbda1451a531d47ba11a28
phylypp <pg_@gmx.net>
2019-02-14 20:11:10 +0100

    workaround for okhttp bug

--- app/src/main/java/de/philipp/housesofgot/api/GoTRepositoryImpl.kt
+++ app/src/main/java/de/philipp/housesofgot/api/GoTRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package de.philipp.housesofgot.api
 
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 /**
  * Repository implementation which loads house data using retrofit.
@@ -10,6 +13,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 class GoTRepositoryImpl : GoTRepository {
 
     private val apiOfIceAndFire: ApiOfIceAndFire = Retrofit.Builder()
+        .client(
+            OkHttpClient.Builder()
+                // workaround for https://github.com/square/okhttp/issues/3146
+                .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                .build()
+        )
         .baseUrl("https://anapioficeandfire.com/api/")
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
